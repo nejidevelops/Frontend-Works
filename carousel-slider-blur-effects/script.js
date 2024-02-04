1fa25e34aaa3aca524c434aeb070d798ba8552a2
Newton Ombese <newtonombese1@gmail.com>
2024-02-04 15:40:12 +0300

    Setting up Javascript

--- carousel-slider-blur-effects/script.js
+++ carousel-slider-blur-effects/script.js
@@ -1,1 +1,20 @@
-console.log('Hello World');
+let nextButton = document.getElementById('next');
+let prevButton = document.getElementById('prev');
+let seeMoreButton = document.querySelectorAll('.seeMore');
+let carousel = document.querySelector('.carousel');
+let listHTML = document.querySelector('.carousel .list');
+
+nextButton.onclick = function() {
+  showSlider('next');
+}
+
+prevButton.onclick = function() {
+  showSlider('prev');
+}
+
+const showSlider = (type) => {
+  let items = document.querySelectorAll('.carousel .list .item');
+  if(type === 'next') {
+    listHTML.appendChild(items[0]);
+  }
+}
